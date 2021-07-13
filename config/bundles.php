34fe35d4589b81307688499ee5179a4d1f72da9f
Alex <alexey.pawlow@whitetigersoft.ru>
2021-07-13 22:16:36 +0300

    composer timestampable-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Andante\TimestampableBundle\AndanteTimestampableBundle::class => ['all' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
